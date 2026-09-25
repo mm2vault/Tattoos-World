7fa561c97b3aacdb719507e010c64aebb8ae8513
mm2vault <huseynoveleddin87@gmail.com>
2026-09-25 14:55:09 +0400

    Pass additional tattoo photos through app

--- src/App.tsx
+++ src/App.tsx
@@ -118,6 +118,7 @@ export default function App() {
     categoryName: string;
     description: string;
     image: string;
+    additionalImages?: string[];
     socialLinks?: {
       instagram?: string;
       tiktok?: string;
