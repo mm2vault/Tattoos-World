0629c1cd4dd18271602c1254e9425a75ccea096f
mm2vault <huseynoveleddin87@gmail.com>
2026-09-26 10:44:59 +0400

    Fix JSX wrapper in Instagram-style app shell

--- src/App.tsx
+++ src/App.tsx
@@ -255,7 +255,6 @@ export default function App() {
                         onToast={(msg) => setToastMessage(msg)}
                         onTattooUpdated={handleTattooUpdated}
                       />
-                    </div>
                   </div>
                 )}
 
